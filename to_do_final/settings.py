1a9bcf06b28d48ff87b313dc0119f0c82f17c38d
Sahil Garg <sahil.garg@crosslynxus.com>
2024-05-07 14:17:13 +0530

    db in supabase

--- to_do_final/settings.py
+++ to_do_final/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'to_do_final.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.qihopewogwvikzkhupro',
+        'PASSWORD': 'Vbnzkinp@1234',
+        'HOST': 'aws-0-ap-south-1.pooler.supabase.com',
+        'PORT': '5432',
     }
 }
 
